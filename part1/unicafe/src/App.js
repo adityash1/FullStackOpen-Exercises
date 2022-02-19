484527312b6df87f892541c1287d11a4e0b3a9ed
adityash1 <aditya167411@gmail.com>
2022-02-19 16:15:20 +0530

    exercise 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+const Statistic = ({ text, value }) => {
+  return (
+    <>
+      <tbody>
+        <tr>
+          <td>{text}: {value}</td>
+        </tr>
+      </tbody>
+    </>
+  );
+};
+
+const Statistics = ({ statistics }) => {
+  const good = statistics[0];
+  const neutral = statistics[1];
+  const bad = statistics[2];
+
+  return (
+    <>
+      <h1>Statistics</h1> 
+      {
+        <>
+          <table>
+            <Statistic text='good' value={good} />
+            <Statistic text='neutral' value={neutral} />
+            <Statistic text='bad' value={bad} />
+          </table>
+        </>
+      }
+    </>
+  );
+};
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setneutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setneutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+
+  return (
+    <>
+      <h1>Give FeedBack</h1>
+      <Button handleClick={handleGood} text='good' />
+      <Button handleClick={handleNeutral} text='neutral' />
+      <Button handleClick={handleBad} text='bad' />
+      <Statistics statistics={[good, neutral, bad]} />
+    </>
+  );
+};
+
+export default App
