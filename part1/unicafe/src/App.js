c93b332da6acbb1d9c81183f9501b8809fb43918
adityash1 <aditya167411@gmail.com>
2022-02-19 17:41:39 +0530

    till exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,22 +16,27 @@ const Statistics = ({ statistics }) => {
   const good = statistics[0];
   const neutral = statistics[1];
   const bad = statistics[2];
-  const total = good + neutral + bad;
+  const total = statistics[3];
 
   const average = () => (good - bad) / total;
   const positive = () => ((good * 100) / total) + '%';
   
   return (
     <>
-      <h1>Statistics</h1> 
-      <table>
-        <Statistic text='good' value={good} />
-        <Statistic text='neutral' value={neutral} />
-        <Statistic text='bad' value={bad} />
-        <Statistic text='all' value={total} />
-        <Statistic text='average' value={average()} />
-        <Statistic text='positive' value={positive()} />
-      </table>
+      <h1>Statistics</h1>
+      {
+        total === 0 ? ('No FeedBack Given') :
+          (
+            <table>
+              <Statistic text='good' value={good} />
+              <Statistic text='neutral' value={neutral} />
+              <Statistic text='bad' value={bad} />
+              <Statistic text='all' value={total} />
+              <Statistic text='average' value={average()} />
+              <Statistic text='positive' value={positive()} />
+            </table>
+          )
+      }
     </>
   );
 };
@@ -44,19 +49,28 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setneutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const handleGood = () => setGood(good + 1);
-  const handleNeutral = () => setneutral(neutral + 1);
-  const handleBad = () => setBad(bad + 1);
+  const handleGood = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+  }
+  const handleNeutral = () => {
+    setneutral(neutral + 1);
+    setTotal(total + 1);
+  }
+  const handleBad = () => {
+    setBad(bad + 1);
+    setTotal(total + 1);
+  }
   
-
   return (
     <>
       <h1>Give FeedBack</h1>
       <Button handleClick={handleGood} text='good' />
       <Button handleClick={handleNeutral} text='neutral' />
       <Button handleClick={handleBad} text='bad' />
-      <Statistics statistics={[good, neutral, bad]} />
+      <Statistics statistics={[good, neutral, bad, total]} />
     </>
   );
 };
