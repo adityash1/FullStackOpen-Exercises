eca3e6fb548553c1078380d46554480b92ed9aa6
adityash1 <aditya167411@gmail.com>
2022-02-24 12:56:02 +0530

    added server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -8,6 +9,14 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState([]);
+
+  useEffect(() => { 
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => 
+        setPersons(response.data) 
+      ) 
+  }, [])  
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
