8c5e6441799f479dfcfdf1adba8a7b88d119eede
adityash1 <aditya167411@gmail.com>
2022-03-13 14:54:26 +0530

    complete note exercise

---
+++ part2/practice-notes/src/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Notes app, Made by Aditya Sharma</em>
+    </div>
+  );
+};
+
+export default Footer
