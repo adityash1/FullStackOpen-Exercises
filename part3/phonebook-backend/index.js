738455b39811d0b388525ca87512b711626b104a
adityash1 <aditya167411@gmail.com>
2022-03-14 17:21:18 +0530

    fixed typo

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -108,7 +108,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
