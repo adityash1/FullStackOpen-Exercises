07b6cc8125e38d26186ae93a295bc9f5e634f646
adityash1 <aditya167411@gmail.com>
2022-03-18 15:12:30 +0530

    connected to database

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,28 +3,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+require("dotenv").config({ path: "./.env" });
+const Person = require("./models/person");
 
 // custom middleware for logging
 // const requestLogger = (request, response, next) => {
@@ -55,59 +35,67 @@ app.get("/index.html", (req, res) => {
   res.send(build / index.html);
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons.map((person) => person.toJSON()));
+  });
 });
 
-app.get("/api/info", (req, res) => {
-  const info = `<p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>`;
-  res.send(info);
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id).then((person) => {
+    if (person) {
+      response.json(person.toJSON());
+    } else {
+      response.status(404).end();
+    }
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id).then(() => {
+    response.status(204).end();
+  });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  person = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
-});
-
-const generateId = () => {
-  return Math.floor(Math.random() * 100000);
-};
-
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
-  if (!body.name || !body.number) {
+  const personName = body.name;
+  const personNumber = body.number;
+
+  if (Object.keys(body).length === 0) {
     return response.status(400).json({
-      error: "name or number missing",
-    });
-  } else if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
+      error: "name or content missing",
     });
   }
+
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => savedPerson.toJSON())
+    .then((savedAndFormattedPerson) => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`);
+      response.json(savedAndFormattedPerson);
+    });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
 
   const person = {
     name: body.name,
     number: body.number,
-    id: generateId(),
   };
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(
+    (updatedPerson) => {
+      response.json(updatedPerson.toJSON());
+    }
+  );
 });
 
 const unknownEndpoint = (request, response) => {
@@ -116,7 +104,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,34 @@
+require("dotenv").config({ path: "../.env" });
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  number: {
+    type: String,
+  },
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
