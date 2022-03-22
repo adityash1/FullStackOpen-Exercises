fb7422a14a819a60b80cf08ed87dc1860218e03b
adityash1 <aditya167411@gmail.com>
2022-03-22 15:41:10 +0530

    exercise 4.8-4.12

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,31 +1,51 @@
 const blogsRouter = require('express').Router()
+require('express-async-errors')
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response, next) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-        .catch(error => next(error))
+// blogsRouter.get('/', (request, response, next) => {
+//     Blog
+//         .find({})
+//         .then(blogs => {
+//             response.json(blogs)
+//         })
+//         .catch(error => next(error))
+// })
+
+blogsRouter.get('/', async (_, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+// blogsRouter.post('/', (request, response, next) => {
+//     const body = request.body
+
+//     const blog = new Blog({
+//         title: body.title,
+//         author: body.author,
+//         url: body.url,
+//         likes: body.likes
+//     })
+
+//     blog
+//         .save()
+//         .then(result => {
+//             response.status(201).json(result)
+//         })
+//         .catch(error => next(error))
+// })
+
+blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
     const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes
+        "title": body.title,
+        "author": body.author,
+        "url": body.url,
+        "likes": body.likes,
     })
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-        .catch(error => next(error))
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
--- part4/models/Blog.js
+++ part4/models/Blog.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+  title: {
+    type: String,
+    required: true
+  },
+  author: String,
+  url: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  }
 })
 
 blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    },
-  })
+  transform: (_, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
 module.exports = mongoose.model('Blog', blogSchema)
