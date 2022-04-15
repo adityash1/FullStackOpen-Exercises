ff980936db95b20c9edff42e5ff1902d0cf5d9a3
adityash1 <aditya167411@gmail.com>
2022-04-16 00:13:29 +0530

    exercise 5.18

--- part5/bloglist-frontend/cypress/integration/blog.app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog.app.spec.js
@@ -1,6 +1,14 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+
+    const user = {
+      'username': 'test',
+      'password': 'test',
+      'name': 'test'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +17,28 @@ describe('Blog app', function () {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('input:first').type('test')
+      cy.get('input:last').type('test')
+      cy.contains('login').click()
+
+      cy.contains('test logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('input:first').type('test')
+      cy.get('input:last').type('wrong')
+      cy.contains('login').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'test logged in')
+    })
   })
 })
