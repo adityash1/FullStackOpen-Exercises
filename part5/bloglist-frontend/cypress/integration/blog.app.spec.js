22956e67d1091216234155e15d50f4b6dbbe0f44
adityash1 <aditya167411@gmail.com>
2022-04-16 00:42:01 +0530

    exercise 5.19

--- part5/bloglist-frontend/cypress/integration/blog.app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog.app.spec.js
@@ -41,4 +41,45 @@ describe('Blog app', function () {
       cy.get('html').should('not.contain', 'test logged in')
     })
   })
+
+  describe.only('When logged in', function () {
+    beforeEach(function () {
+      cy.get('input:first').type('test')
+      cy.get('input:last').type('test')
+      cy.contains('login').click()
+    })
+
+    it('Blog form is shown', function () {
+      cy.contains('new blog')
+      cy.contains('contents')
+    })
+
+    it('Create new blog form is shown', function () {
+      cy.contains('new blog').click()
+      cy.contains('title')
+      cy.contains('author')
+      cy.contains('url')
+      cy.contains('cancel').click()
+    })
+
+    it.only('A blog can be created', function () {
+      const blog = {
+        'title': 'React patterns',
+        'author': 'Michael Chan',
+        'url': 'https://reactpatterns.com/'
+      }
+
+      cy.contains('new blog').click()
+      cy.get('#title').type(blog.title)
+      cy.get('#author').type(blog.author)
+      cy.get('#url').type(blog.url)
+      cy.get('#create-button').click()
+
+      cy.get('.blogTitle').should('contain', blog.title)
+      cy.get('.success')
+        .should('contain', 'a new blog React patterns by Michael Chan')
+        .and('have.css', 'color', 'rgb(0, 128, 0)')
+        .and('have.css', 'border-style', 'solid')
+    })
+  })
 })
--- part5/bloglist-frontend/src/components/CreateBlog.js
+++ part5/bloglist-frontend/src/components/CreateBlog.js
@@ -27,7 +27,7 @@ const CreateBlog = ({ addBlog }) => {
         <div>title: <input id='title' value={title} onChange={({ target }) => setTitle(target.value)} /></div>
         <div>author: <input id='author' value={author} onChange={({ target }) => setAuthor(target.value)} /></div>
         <div>url: <input id='url' value={url} onChange={({ target }) => setUrl(target.value)} /></div>
-        <button type="submit">create</button>
+        <button id='create-button' type='submit'>create</button>
       </form>
     </div>
   )
