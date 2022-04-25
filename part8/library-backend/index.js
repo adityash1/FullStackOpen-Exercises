4644d309c18ad5246c3e8e138c957049c2c3bdd5
adityash1 <aditya167411@gmail.com>
2022-04-26 01:52:05 +0530

    exercise 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -80,8 +80,10 @@ let books = [
 
 const typeDefs = gql`
   type Author {
+    id: ID!
     name: String!
-    bookCount: Int
+    bookCount: Int!
+    born: Int
   }
   type Book {
     id: ID!
@@ -93,7 +95,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -102,7 +104,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter((book) => book.author === args.author)
+    },
     allAuthors: (root) => authors,
   },
   Author: {
