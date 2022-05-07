ac6e79e5cbd59b95fd606ff75f5475a67fc6190d
adityash1 <aditya167411@gmail.com>
2022-05-07 15:35:12 +0530

    exercise 9.12

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -6,4 +6,13 @@ patientRouter.get("/", (_req : any, res : any) => {
   res.send(service.getPatients());
 });
 
+patientRouter.post('/', (req: any, res: any) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const newEntry = service.addPatient(
+    name, dateOfBirth, ssn, gender, occupation
+  );
+
+  res.json(newEntry);
+});
+
 export default patientRouter;
--- part9/patientor-backend/src/services/patientService.ts
+++ part9/patientor-backend/src/services/patientService.ts
@@ -1,18 +1,33 @@
 import data from "../../data/patients.json";
-import { Patient } from '../types';
+import { Patient } from "../types";
+
+import { v4 as uuidv4 } from "uuid";
 
 const patients: Array<Patient> = data as Array<Patient>;
 
-const getPatients = (): Omit<Patient, 'ssn'>[] => {
+const getPatients = (): Omit<Patient, "ssn">[] => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
     id,
     name,
     dateOfBirth,
     gender,
-    occupation
+    occupation,
   }));
 };
 
+const addPatient = (
+  name: string,
+  dateOfBirth: string,
+  ssn: string,
+  gender: "male" | "female" | "other",
+  occupation: string
+): Patient => {
+  const id: string = uuidv4();
+  const newEntry = { id, name, dateOfBirth, ssn, gender, occupation };
+  return newEntry;
+};
+
 export default {
-  getPatients
+  getPatients,
+  addPatient,
 };
