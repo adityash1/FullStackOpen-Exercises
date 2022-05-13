f6a17e548be3294bf4d460ca2d52d3ec2ee0f21e
adityash1 <aditya167411@gmail.com>
2022-05-13 17:25:40 +0530

    Exercise 10.7: scrollable app bar

--- part10/rate-repository-app/src/components/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -1,12 +1,12 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
+
 import AppBarTab from "./AppBarTab";
 import theme from "../theme";
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    justifyContent: "space-between",
     padding: 20,
     paddingTop: 20 + Constants.statusBarHeight,
     backgroundColor: theme.colors.black,
@@ -17,8 +17,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab to="/">Repositories</AppBarTab>
-      <AppBarTab to="/signin">Sign in</AppBarTab>
+      <ScrollView horizontal>
+        <AppBarTab to="/">Repositories</AppBarTab>
+        <AppBarTab to="/signin">Sign in</AppBarTab>
+      </ScrollView>
     </View>
   );
 };
--- part10/rate-repository-app/src/components/AppBarTab.jsx
+++ part10/rate-repository-app/src/components/AppBarTab.jsx
@@ -1,13 +1,22 @@
-import { Pressable } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import { Link } from "react-router-native";
 
 import Text from "./Text";
+
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  tab: {
+    color: theme.colors.white,
+    marginLeft: 20,
+  },
+});
 
 const AppBarTab = ({ to, children }) => {
   return (
     <Pressable>
       <Link to={to}>
-        <Text heading bold style={{ color: "#fff" }}>
+        <Text heading bold style={styles.tab}>
           {children}
         </Text>
       </Link>
